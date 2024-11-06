5afe39edd4f18e8359ec463f51b45233fcc1547f
Tim Carlson <timca@arenasports.net>
2024-11-06 14:12:35 -0800

    update react-chat-2 in starter branch with commented out sample code

--- react-chat-2/src/components/ChannelList.jsx
+++ react-chat-2/src/components/ChannelList.jsx
@@ -1,28 +1,67 @@
 import React from 'react';
 
 export function ChannelList(props) {
-  const {channelNames, currentChannel} = props;
 
-  const elemArray = channelNames.map((channelNameString) => {
-    let classListString = "px-2";
-    if(channelNameString === currentChannel) { //on current channel
-      classListString += " bg-warning";
+  const channels = props.channels;
+  const currentChannel = props.currentChannel;
+
+  const linkElemArray = channels.map((channelNameString) => {
+
+    let classList = "btn btn-outline-light my-1";
+    if(channelNameString === currentChannel) {
+      classList = "btn btn-warning"
     }
 
-    const transformed = (
-      <li className={classListString} key={channelNameString}>
-        <a href={"/"+channelNameString}>{channelNameString}</a>
-      </li>
-    );
-
-    return transformed;
+    const element = (
+      <div key={channelNameString}>
+        <button className={classList} href="">{channelNameString}</button>
+      </div>
+    )
+    return element;
   })
 
   return (
-    <nav className="channel-nav h-100 bg-secondary px-0 py-3">
-      <ul className="px-0">
-        {elemArray}
-      </ul>
+    <nav className="text-light bg-secondary h-100 py-3 channel-nav px-2">
+      {linkElemArray}
     </nav>
   )
 }
+
+// //Post Lecture, add the Channel navigation (elevate state, etc)
+// import React from 'react';
+
+// export function ChannelList(props) {
+
+//   const channels = props.channels;
+//   const currentChannel = props.currentChannel;
+//   const howToChangeTheChannel = props.howToChangeTheChannel;
+
+//   const handleClick = (event) => {
+//     event.preventDefault();
+//     const chosenChannel = event.target.name;
+//     console.log(chosenChannel)
+//     howToChangeTheChannel(chosenChannel);
+
+//   }
+
+//   const linkElemArray = channels.map((channelNameString) => {
+
+//     let classList = "btn btn-outline-light my-1";
+//     if (channelNameString === currentChannel) {
+//       classList = "btn btn-warning"
+//     }
+
+//     const element = (
+//       <li key={channelNameString}>
+//         <a name={channelNameString} onClick={handleClick} className={classList} href="">{channelNameString}</a>
+//       </li>
+//     )
+//     return element;
+//   })
+
+//   return (
+//     <nav className="text-light bg-secondary h-100 py-3 channel-nav px-2">
+//       {linkElemArray}
+//     </nav>
+//   )
+// }
