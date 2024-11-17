cbc573ade05459492d81718588a8366900e60675
Tim Carlson <timca@arenasports.net>
2024-11-17 12:16:11 -0800

    fix couple syntax issues in starter of react-browserrouter starter branch

--- react-chat-router/src/main.jsx
+++ react-chat-router/src/main.jsx
@@ -12,6 +12,7 @@ createRoot(document.getElementById('root')).render(
 
 // // Slide 16
 
+// import React from 'react';
 // import { createRoot } from 'react-dom/client'
 // import { BrowserRouter } from 'react-router-dom';
 // import App from './components/App.jsx';
